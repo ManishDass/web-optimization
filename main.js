9087fd81c304c5ced2a40984636fad3cd50ce6d2
Manish <manishdas11811480@gmail.com>
2023-01-30 17:49:44 +0530

    fix

--- main.js
+++ main.js
@@ -5,45 +5,47 @@ const path = require('path');
 const cache = require('cache-headers');
 const port = process.env.PORT || 3000;
 
-const pathsConfig = {
-  paths: {
-      '/**/generic': {
-          staleRevalidate: 'ONE_HOUR',
-          staleError: 'ONE_HOUR'
-      },
-      '/default/values': {},
-      '/user/route': false,
-      '/**': 60
-  }
-};
+// const pathsConfig = {
+//   "maxAge": 2000,
+//   // paths: {
+//   //     '/**/generic': {
+//   //         staleRevalidate: 'ONE_HOUR',
+//   //         staleError: 'ONE_HOUR'
+//   //     },
+//   //     '/default/values': {},
+//   //     '/user/route': 60,
+//   //     '/**': 60
+//   // }
+// };
 
 
-const app = express();const shouldCompress = (req, res) => {
-  if (req.headers['x-no-compression']) {
-    // Will not compress responses, if this header is present
-    return false;
-  }
-  // Resort to standard compression
-  return compression.filter(req, res);
-};
+const app = express();
+// const shouldCompress = (req, res) => {
+//   if (req.headers['x-no-compression']) {
+//     // Will not compress responses, if this header is present
+//     return false;
+//   }
+//   // Resort to standard compression
+//   return compression.filter(req, res);
+// };
 
-// Compress all HTTP responses
-app.use(compression({
-  // filter: Decide if the answer should be compressed or not,
-  // depending on the 'shouldCompress' function above
-  filter: shouldCompress,
-  // threshold: It is the byte threshold for the response 
-  // body size before considering compression, the default is 1 kB
-  threshold: 0
-}));
+// // Compress all HTTP responses
+// app.use(compression({
+//   // filter: Decide if the answer should be compressed or not,
+//   // depending on the 'shouldCompress' function above
+//   filter: shouldCompress,
+//   // threshold: It is the byte threshold for the response 
+//   // body size before considering compression, the default is 1 kB
+//   threshold: 0
+// }));
 
-app.use(cache.setupInitialCacheHeaders(pathsConfig));
+// app.use(cache.setupInitialCacheHeaders(pathsConfig));
 
 app.use('/', express.static('./public'));
 
-app.use(function (req, res, next) {
-  res.set('Cache-control', 'public, max-age=300')
-})
+// app.use(function (req, res, next) {
+//   res.set('Cache-control', 'public, max-age=300')
+// })
 
 app.get('/', (req, res) => {
   const animal = 'elephant';
