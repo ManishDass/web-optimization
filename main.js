0f4fb16d1dbe1fefd3377f1542323e6fa083d3e0
Manish <manishdas11811480@gmail.com>
2023-01-30 17:55:06 +0530

    fix gzip

--- main.js
+++ main.js
@@ -20,24 +20,25 @@ const port = process.env.PORT || 3000;
 
 
 const app = express();
-// const shouldCompress = (req, res) => {
-//   if (req.headers['x-no-compression']) {
-//     // Will not compress responses, if this header is present
-//     return false;
-//   }
-//   // Resort to standard compression
-//   return compression.filter(req, res);
-// };
 
-// // Compress all HTTP responses
-// app.use(compression({
-//   // filter: Decide if the answer should be compressed or not,
-//   // depending on the 'shouldCompress' function above
-//   filter: shouldCompress,
-//   // threshold: It is the byte threshold for the response 
-//   // body size before considering compression, the default is 1 kB
-//   threshold: 0
-// }));
+const shouldCompress = (req, res) => {
+  if (req.headers['x-no-compression']) {
+    // Will not compress responses, if this header is present
+    return false;
+  }
+  // Resort to standard compression
+  return compression.filter(req, res);
+};
+
+// Compress all HTTP responses
+app.use(compression({
+  // filter: Decide if the answer should be compressed or not,
+  // depending on the 'shouldCompress' function above
+  filter: shouldCompress,
+  // threshold: It is the byte threshold for the response 
+  // body size before considering compression, the default is 1 kB
+  threshold: 0
+}));
 
 // app.use(cache.setupInitialCacheHeaders(pathsConfig));
 
